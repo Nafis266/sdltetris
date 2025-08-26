a362d2db2c7e73e86a0cbe8eeabc8f0c3a2b16d0
Nafis266 <nafis.niyas@gmail.com>
2025-08-27 00:18:31 +0530

    added multiple blocks with controls

--- tetris.c
+++ tetris.c
@@ -4,17 +4,29 @@
 #include<unistd.h>
 #include<stdbool.h>
 
+#define SHAPECOUNT 10
+
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
-static int xval = 100;
-static int yval = 100;
+static int xval[SHAPECOUNT];
+static int yval[SHAPECOUNT];
 static int diff = 100;
-static int i=0;
-static bool movdown = true;
+static int frames=0;
+static bool movdown[SHAPECOUNT];
+static int cshape = 0;
+
+void initializearr(){
+	for(int i=0;i<SHAPECOUNT;i++){
+		xval[i] = 300;
+		yval[i] = -100;
+		movdown[i] = true;
+	}
+}
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argb[]){
 	window = SDL_CreateWindow("tetris", 700,700,0);
 	renderer = SDL_CreateRenderer(window,NULL);
+	initializearr();
 	return SDL_APP_CONTINUE;
 }
 
@@ -23,12 +35,12 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
 		return SDL_APP_SUCCESS;
 	}if(event->type == SDL_EVENT_KEY_DOWN){
 		if(event->key.key == SDLK_RIGHT){
-			if(xval<600 && movdown){
-				xval+=100;
+			if(xval[cshape]<600 && movdown[cshape]){
+				xval[cshape]+=100;
 			}
 		}else if(event->key.key == SDLK_LEFT){
-			if(xval>0 && movdown){
-				xval-=100;
+			if(xval[cshape]>0 && movdown[cshape]){
+				xval[cshape]-=100;
 			}
 		}
 	}
@@ -37,20 +49,24 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
 
 SDL_AppResult SDL_AppIterate(void *appstate){
 	SDL_RenderClear(renderer);
-	SDL_Vertex rectvert[4] = {
-	    { {xval,yval}, {255,0,0,0}, {0, 0} }, 
-	    { {xval+diff, yval}, {255,0,0,0}, {1, 0} }, 
-	    { {xval+diff, yval+diff}, {255,0,0,0}, {1, 1} },
-	    { {xval, yval+diff}, {255,0,0,0}, {0,1}},
-	};
+
 	int index[6] = {0,1,2,0,2,3};
-	SDL_RenderGeometry(renderer,NULL,rectvert,4,index,6);
+	for(int shape = 0; shape < SHAPECOUNT; shape++){
+		SDL_Vertex rectvert[4] = {
+		{ {xval[shape],yval[shape]}, {255,0,0,0}, {0, 0} },
+		{ {xval[shape]+diff, yval[shape]}, {255,0,0,0}, {1, 0} },
+		{ {xval[shape]+diff, yval[shape]+diff}, {255,0,0,0}, {1, 1} },
+		{ {xval[shape], yval[shape]+diff}, {255,0,0,0}, {0,1}},
+		};
+		SDL_RenderGeometry(renderer,NULL,rectvert,4,index,6);
+	}
 	SDL_RenderPresent(renderer);
-	if(movdown && i++>2000){
-		yval+=100;
-		i=0;
-		if(yval>500){
-			movdown=false;
+	if(movdown[cshape] && frames++>2000){
+		yval[cshape]+=100;
+		frames=0;
+		if(yval[cshape]>500){
+			movdown[cshape]=false;
+			cshape++;
 		}
 	}
 	return SDL_APP_CONTINUE;
